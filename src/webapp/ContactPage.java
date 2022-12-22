875c6afc51ff428b7818918b358514c19894696a
taylo <tayloralexisandres@gmail.com>
2022-12-22 14:54:14 -0700

    Circle area

---
+++ src/webapp/ContactPage.java
@@ -0,0 +1,16 @@
+package webapp;
+
+public class ContactPage {
+    public static void main(String[] args) {
+
+
+
+    int radius = 6;
+    int PI = (int) 3.14;
+    double area= radius*2*PI;
+
+        System.out.println(area);
+
+}
+
+}
