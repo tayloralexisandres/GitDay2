f7eb75d37abc811d6b22c2e38468fbd553fd78fc
taylo <tayloralexisandres@gmail.com>
2023-01-03 09:57:36 -0700

    TAy02 updated homepage

--- src/webapp/ContactPage.java
+++ src/webapp/ContactPage.java
@@ -1,16 +1,6 @@
 package webapp;
 
 public class ContactPage {
-    public static void main(String[] args) {
 
-
-
-    int radius = 6;
-    int PI = (int) 3.14;/// this is explictly casted
-    double area= radius*2*PI;
-
-        System.out.println(area);//
-
-}
 
 }
--- src/webapp/Homepage.java
+++ src/webapp/Homepage.java
@@ -1,17 +1,18 @@
 package webapp;
 
 public class Homepage {
-
     public static void main(String[] args) {
 
-        int a=6;//4
-        int b=4;
-        int c=0;//6
-        c=a;//6
-        a=b;//4
-        b=c;//6
-        System.out.println(a+" "+b);
 
+        int a = 6;//4
+        int b = 4;
+        int c = 0;//6
+        c = a;//6
+        a = b;//4
+        b = c;//6
+        System.out.println("a= "+a+", b="+b);
 
     }
+
+
 }
